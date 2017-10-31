6d3f09b34d92e6a644eefbd09f58e3b3bac24623
SunmenLone <sunmenlone@icloud.com>
2017-10-31 12:02:20 +0800

    Integrate Druid component

--- src/main/java/com/jybl/admin/AdminApplication.java
+++ src/main/java/com/jybl/admin/AdminApplication.java
@@ -2,8 +2,10 @@ package com.jybl.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class AdminApplication {
 
 	public static void main(String[] args) {
